e756cdd8f894305080dafa83576f7ae85e55d0d6
Anderson Truong <andersonleetruong@gmail.com>
2023-02-16 15:21:11 -0800

    Finished SearchBar for courses

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,12 +2,14 @@ import { Autocomplete, Paper, TextField, InputAdornment, Box } from "@mui/materi
 import SearchIcon from '@mui/icons-material/Search';
 import { useState } from "react";
 import { queryCoursePrefix } from "../database/mongodb";
+import { UserAuth } from "../context/AuthContext";
 
-export default function SearchBar({props}) {
+export default function SearchBar(props) {
     const [courses, setCourses] = useState([]);
     const [inputTextValue, setInputTextValue] = useState('');
     const [optionIndex, setOptionIndex] = useState(0);
     const [mouseOver, setMouseOver] = useState(false);
+    const { user } = UserAuth();
 
     const sortByWordPrefix = (data, prefix) => {
         prefix = prefix.trim();
@@ -52,33 +54,49 @@ export default function SearchBar({props}) {
         return sortedData;
     }
 
-    const searchPrefix = async (prefix) => {
-        if (prefix.length === 1)
-            localStorage.removeItem('courseLabels');
-        // First check if in local storage
-        prefix = prefix.toLowerCase().trim();
-        const oldData = JSON.parse(localStorage.getItem('courseLabels'));
-        if (oldData && oldData.length >= 5) {
-            const filteredOldData = oldData.filter((doc) => doc.label.toLowerCase().includes(prefix));
-            setCourses(sortByWordPrefix(filteredOldData, prefix).slice(0, 5));
+    /**
+     * Searches the local storage or queries a database for objects with a
+     * property containing a word
+     * @param {*} word Word to be matched against
+     */
+    const searchByWord = async (word) => {
+        // Clear cache when search is empty
+        if (word.length === 0) {
+            localStorage.removeItem(props.cachename);
             return;
         }
-        const newData = await queryCoursePrefix(prefix);
+
+        // First check if cached
+        word = word.toLowerCase();
+        const oldData = JSON.parse(localStorage.getItem(props.cachename));
+        if (oldData && oldData.length >= 5) {
+            const filteredOldData = oldData.filter((doc) => doc.label.toLowerCase().includes(word));
+            setCourses(sortByWordPrefix(filteredOldData, word).slice(0, 5));
+            return;
+        }
+
+        // Query if not cached
+        const jwt = user?.accessToken;
+        const newData = await queryCoursePrefix(word, jwt);
         for (const data of newData) {
             data.label = data.subjectLabel + ' ' + data.number + ' ' + data.name;
         }
-        setCourses(sortByWordPrefix(newData, prefix).slice(0, 5));
-        localStorage.setItem('courseLabels', JSON.stringify(newData));
+        setCourses(sortByWordPrefix(newData, word).slice(0, 5));
+        localStorage.setItem(props.cachename, JSON.stringify(newData));
     }
 
+    /**
+     * Renders the SearchBar input field base
+     * @param {*} params TextField params
+     * @returns {Paper} `Paper` component
+     */
     const SearchBarInputBase = (params) => {
         return (
         <Paper
             component="form"
             sx={{ display: 'flex', alignItems: 'center' }}
-            ref={params.InputProps.ref}
-        >
-            
+            ref={params.InputProps.ref}>
+
             <TextField
                 {...params}
                 variant="standard"
@@ -88,10 +106,17 @@ export default function SearchBar({props}) {
                 inputProps={params.inputProps}
                 InputProps={{...params.InputProps, disableUnderline: true, startAdornment: <InputAdornment position="start"><SearchIcon /></InputAdornment>}}
             />
+
         </Paper>
         );
     }
 
+    /**
+     * Handles different key interactions with SearchBar dropdown
+     * If the key is a 'TAB', the selected row is moved down. If the current selected row is at the end, it loops around.
+     * If the key is an 'ENTER', the SearchBar text is replaced with the selected row's text.
+     * @param {KeyboardEvent} event Key Event
+     */
     const handleKeyDown = (event) => {
         switch (event.key) {
             case 'Tab':
@@ -102,11 +127,23 @@ export default function SearchBar({props}) {
                 event.preventDefault();
                 setInputTextValue(courses[optionIndex]);
                 break;
+            // BUG If you press arrow keys, unintended behavior. FUTURE FIX
+            case 'ArrowUp':
+            case 'ArrowDown':
+            case 'ArrowRight':
+            case 'ArrowLeft':
+                setOptionIndex(-1);
             default:
                 break;
         }
     }
 
+    /**
+     * Handles rendering each row in the SearchBar dropdown
+     * @param {*} props The props to apply to the `li` element
+     * @param {*} option The option to render
+     * @returns {JSX.IntrinsicElements.li} `li` element
+     */
     const handleRenderOption = (props, option) => {
         return (
         <li {...props} 
@@ -132,15 +169,19 @@ export default function SearchBar({props}) {
                 PaperComponent={(props) => <Paper elevation={10} {...props} />}
                 autoComplete={true}
                 options={courses}
-                sx={{ width: 600 }}
+                sx={{ width: props.width }}
                 onKeyDown={handleKeyDown}
-                renderInput={SearchBarInputBase
-              }
-                onInputChange={(e, value) => {searchPrefix(value); setInputTextValue(value);} }
+                renderInput={SearchBarInputBase}
+                onInputChange={(e, value) => {searchByWord(value); setInputTextValue(value);} }
                 onChange={(e, value) => setCourses(value ? [value] : [])}
                 renderOption={handleRenderOption}
             >
             </Autocomplete>
         </div>
     );
+}
+
+SearchBar.defaultProps = {
+    width: 300,
+    cachename: 'labels'
 }
