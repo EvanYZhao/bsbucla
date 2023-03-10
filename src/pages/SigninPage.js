71f203be4b5c4badab2dad4c4a8dbd49637991dc
Scott Park <scottpark03@gmail.com>
2023-03-09 18:32:57 -0800

    styling for the sign in page and added a styles folder to src

--- src/pages/SigninPage.js
+++ src/pages/SigninPage.js
@@ -3,7 +3,30 @@ import { GoogleButton } from "react-google-button";
 import { UserAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline,Grid, Toolbar, Container } from "@mui/material";
+// import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+// import Avatar from '@mui/material/Avatar';
+// import Button from '@mui/material/Button';
+// import TextField from '@mui/material/TextField';
+// import FormControlLabel from '@mui/material/FormControlLabel';
+// import Checkbox from '@mui/material/Checkbox';
+// import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
+// import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import App from "../App";
+import { PeopleAlt } from "@mui/icons-material";
+import { ClassNames } from "@emotion/react";
+
+
+const theme = createTheme();
+
+
+
 export default function SigninPage() {
+
+
   const { googleSignIn, user } = UserAuth();
   const navigate = useNavigate();
 
@@ -24,5 +47,26 @@ export default function SigninPage() {
     }
   }, [user]);
 
-  return <GoogleButton onClick={handleSignIn} />;
+
+  return (
+
+    <div>
+
+          <div class = 'centered' >
+            
+              <Container height = '10em' display =  'flex' align =  'center' justify-content ='center' >
+                <img src="https://i.imgur.com/cvZnH8a.png" alt="BSB" width = '450'/>
+            
+                <GoogleButton onClick={handleSignIn} type = "light" />
+              </Container>
+           
+
+        </div>
+
+   
+    </div>
+
+
+
+  );
 }
