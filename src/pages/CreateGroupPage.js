142d90e1eda4d23c22839b9fc4060345f467a026
Theo Zhang <azhang8@g.ucla.edu>
2023-03-12 13:01:30 -0700

    Adding handle change functions to save information

--- src/pages/CreateGroupPage.js
+++ src/pages/CreateGroupPage.js
@@ -5,29 +5,65 @@ import {
   TextField,
   Typography
 } from "@mui/material";
+import GroupCard from "../components/GroupCard";
+import {
+  queryCourseFromId,
+  queryGroupsFromCourseId,
+  queryGroupFromId,
+  joinGroupById,
+  leaveGroupById,
+  createGroup,
+} from "../database/mongodb";
+import { useParams } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
 
 export default function CreateGroup() {
   //todo:
-  //create a form for the user to type in their group requirements
   //group name, class, number of people max in group, a description
-  //on submit:
-  //save all the info and check it:
-  //check if the groupname has been taken already
-  //if it's been taken, dipslay message/highlight the name box and say it's been taken
-  //message goes away once they resubmit and it's fine
-  //if all data is valid, call the create group api with the given info
-  //display a card that displays all the resulting information and a link to the group's page
-
-  //for choosing the class to create the group for, do the autofill thing and save the class id for the api call later
-  //on submit, have to check if all data is valid and filled out
+  //NEED HELP: for choosing the class to create the group for, do the autofill thing and save the class id for the api call later
+  const { id } = useParams();
+  const { user } = UserAuth();
 
   const [nameTaken, setNameTaken] = useState(false);
+  const [allValid, setAllValid] = useState(true);
+
+  const [groupName, setGroupName] = useState("");
+  const [className, setClassName] = useState("");
+  const [maxPeople, setMaxPeople] = useState(0);
+  const [description, setDescription] = useState("");
   
   const joinButtonHandler = async () =>{
     //do all checks for validity here
-    //once all valid, call create group and set validity variables to true
-    //if not valid, set the validity variables to false
-    //should i do useeffect here to reset the page?
+    //once all valid, call create group, reset form to empty, and display a clickable card that routes to their newly create group page 
+      //also displays a message that says "you can view your groups on the home page" or something like that
+    //if not valid, set the validity variables to false 
+    //should i do useeffect here to reset the page to make the values display as false?
+    
+    /*if (allValid){
+      //call create group
+      groupData = createGroup
+      return(
+        GroupCard(groupData._id)
+      );
+    }*/
+    //NEED HELP: displaying the card underneath
+  };
+
+  const handleNameChange = e =>{
+    setGroupName(e.target.value);
+  };
+
+  const handleClassNameChange = e =>{
+    setClassName(e.target.value);
+    //want to do the search bar like thing here but not sure how to get the id from that
+  };
+
+  const handleMaxPeopleChange = e =>{
+    setMaxPeople(e.target.value);
+  };
+
+  const handleDescriptionChange = e =>{
+    setDescription(e.target.value);
   };
 
   return (
@@ -45,15 +81,18 @@ export default function CreateGroup() {
         <TextField
           helperText= {nameTaken ? "Name already taken":" "}
           label="Group Name"
+          onChange={handleNameChange}
         />
         <TextField
           helperText=" "
           label="Class"
+          onChange={handleClassNameChange}
         />
         <TextField
           helperText=" " 
           label="Group size"
           type="number"
+          onChange={handleMaxPeopleChange}
         />
       </Box>
       <TextField
@@ -61,10 +100,10 @@ export default function CreateGroup() {
           label="Group description"
           multiline
           rows={4}
-          maxRows={10}
           sx={{
             width:'500px'
           }}
+          onChange={handleDescriptionChange}
         />
       <Button
         sx={{
@@ -73,7 +112,9 @@ export default function CreateGroup() {
           fontSize: '20px'
         }}
         onClick={joinButtonHandler}
-      > Create group </Button>
+      > 
+        Create group 
+      </Button>
     </div>
   );
 }
