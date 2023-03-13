d626ef7dd74d9d9c0d0e817535eb7c2a2f751786
Theo Zhang <azhang8@g.ucla.edu>
2023-03-12 18:28:29 -0700

    Catching error message for creating a group

--- src/pages/CreateGroupPage.js
+++ src/pages/CreateGroupPage.js
@@ -7,11 +7,6 @@ import {
 } from "@mui/material";
 import GroupCard from "../components/GroupCard";
 import {
-  queryCourseFromId,
-  queryGroupsFromCourseId,
-  queryGroupFromId,
-  joinGroupById,
-  leaveGroupById,
   createGroup,
 } from "../database/mongodb";
 import { useParams } from "react-router-dom";
@@ -19,43 +14,32 @@ import { UserAuth } from "../context/AuthContext";
 
 export default function CreateGroup() {
   //todo:
-  //group name, class, number of people max in group, a description
-  //NEED HELP: for choosing the class to create the group for, do the autofill thing and save the class id for the api call later
+  //group name, class ID, number of people max in group, a description
+  //for simplicity, make the user get the class ID rather than looking for it?
+  //or let them type in the class name? and do a search bar type of deal
   const { id } = useParams();
   const { user } = UserAuth();
 
-  const [nameTaken, setNameTaken] = useState(false);
-  const [allValid, setAllValid] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const [groupName, setGroupName] = useState("");
-  const [className, setClassName] = useState("");
+  const [classId, setclassId] = useState("");
   const [maxPeople, setMaxPeople] = useState(0);
   const [description, setDescription] = useState("");
   
   const joinButtonHandler = async () =>{
-    //do all checks for validity here
-    //once all valid, call create group, reset form to empty, and display a clickable card that routes to their newly create group page 
-      //also displays a message that says "you can view your groups on the home page" or something like that
-    //if not valid, set the validity variables to false 
-    //should i do useeffect here to reset the page to make the values display as false?
+    const response = createGroup(user?.accessToken, {name: groupName, desc: description, 
+      courseId: classId, maxMembers: maxPeople}).then(result => result)
+      .catch(err => setErrorMessage(err))
     
-    /*if (allValid){
-      //call create group
-      groupData = createGroup
-      return(
-        GroupCard(groupData._id)
-      );
-    }*/
-    //NEED HELP: displaying the card underneath
   };
 
   const handleNameChange = e =>{
     setGroupName(e.target.value);
   };
 
-  const handleClassNameChange = e =>{
-    setClassName(e.target.value);
-    //want to do the search bar like thing here but not sure how to get the id from that
+  const handleclassIdChange = e =>{
+    setclassId(e.target.value);
   };
 
   const handleMaxPeopleChange = e =>{
@@ -79,24 +63,20 @@ export default function CreateGroup() {
         }}
       >
         <TextField
-          helperText= {nameTaken ? "Name already taken":" "}
           label="Group Name"
           onChange={handleNameChange}
         />
         <TextField
-          helperText=" "
           label="Class"
-          onChange={handleClassNameChange}
+          onChange={handleclassIdChange}
         />
         <TextField
-          helperText=" " 
           label="Group size"
           type="number"
           onChange={handleMaxPeopleChange}
         />
       </Box>
       <TextField
-          helperText=" "
           label="Group description"
           multiline
           rows={4}
