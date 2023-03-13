33d198080bcc97b7294eeb6d94cfb9b7ee18dc1c
anderson-truong <andersonleetruong@gmail.com>
2023-03-12 18:46:32 -0700

    Added Messaging with WebSockets

---
+++ src/components/ChatBox.js
@@ -0,0 +1,11 @@
+
+
+const socketPath = "https://bsbucla-chat.up.railway.app";
+export default function ChatBox() {
+    const { user } = UserAuth();
+    return (
+        <div>
+
+        </div>
+    )
+}
