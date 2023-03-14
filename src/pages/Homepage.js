dc5629a90934d102d10326b40ec5abde9902fb82
AnooshkhaShetty <anooshkhashetty@gmail.com>
2023-03-13 22:26:52 -0700

    Styled the homepage. Changed the colors and fonts, added rounded backgrounds, and centered everything.

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -6,11 +6,11 @@ import { UserAuth } from "../context/AuthContext";
 import Typography from "@mui/material/Typography";
 import GroupCard from "../components/GroupCard";
 import { getUserProfile} from "../database/mongodb";
-import { createTheme} from '@mui/material/styles';
+import { createTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 
-const theme = createTheme({
-
-});
+const theme = createTheme();
 
 export default function Homepage() {
   const { user } = UserAuth(); // Used to display user name
@@ -24,34 +24,41 @@ export default function Homepage() {
   }, [user]);
 
   return (
-    <div className="bg-slate-100 h-full flex flex-col items-center" theme ={theme}>
+    <div className="bg-slate-100 h-full flex flex-col items-center">
       <div
         id="navTitle"
-        className="w-full
-                  h-20
-                  flex
-                  justify-center
-                  items-center
-           "
+        className="w-full h-20 flex justify-center items-center"
       >
-        <Typography fontFamily = "Manrope, sans-serif" fontSize = "4.5rem" color = "#3a586b">
-         <b> BRUIN STUDY BUDDIES </b>
-        </Typography>
+        <Box bgcolor="#e4ecf0" borderRadius={4} p={2}>
+          <Typography fontFamily="Manrope, sans-serif" fontSize="4.5rem" color="#3a586b">
+            <b>BRUIN STUDY BUDDIES</b>
+          </Typography>
+        </Box>
       </div>
-
-      <div className="grid grid-cols-2 w-full px-24">
-        <div className="flex flex-col space-y-6 items-center">
-          <Typography fontFamily = "Manrope, sans-serif" color = "#3a586b"  variant="h5"> 
-          CHATBOX </Typography>
-        </div>
-        <div className="flex flex-col space-y-6 items-center">
-          <Typography fontFamily = "Manrope, sans-serif" color = "#3a586b" variant="h5">GROUPS</Typography>
-          <div className="w-1/2 space-y-4">
-            {groups?.map((g) => (
-              <div key={listId}>
-                <GroupCard id={listId} groupID={g} />
-              </div>
-            ))}
+      <div className="w-full flex justify-center">
+        <div className="w-3/4 grid grid-cols-2 gap-4">
+          <div className="flex flex-col space-y-6 items-center">
+            <Typography fontFamily="Manrope, sans-serif" fontSize="2rem" color="#3a586b"> 
+              CHATBOX
+            </Typography>
+          </div>
+          <div className="flex flex-col space-y-6 items-center">
+            <Typography fontFamily="Manrope, sans-serif" fontSize="2rem" color="#3a586b">GROUPS</Typography>
+            <Box
+              bgcolor="#e4ecf0"
+              marginTop={2}
+              borderRadius={4}
+              p={2.5}
+              mb={3}
+            >
+              <Grid container spacing={2}>
+                {groups?.map((g) => (
+                  <Grid item xs={12} sm={6} key={listId}>
+                    <GroupCard id={listId} groupID={g} />
+                  </Grid>
+                ))}
+              </Grid>
+            </Box>
           </div>
         </div>
       </div>
